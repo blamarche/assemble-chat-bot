b048d526f5e952d97e7984c9e27d1b2ebd34bd83
Brendan L <drac@dracsoft.com>
2017-02-06 21:48:25 -0500

    search wikipedia if no ddg result

--- bots/search.js
+++ bots/search.js
@@ -26,43 +26,37 @@ exports.onmessage = function(socket, mdata) {
         var results = "";
         var m="";
 
-        https.get('https://api.duckduckgo.com/?t=chatassistsearch&format=json&q='+s, function(res) {
-            if (res.statusCode !== 200) {
-                socket.emit('chatm', JSON.stringify({"t": token, "room": mdata.room, "m": "Search server error.", "dur":"1m"}));
+        _gethttps('https://api.duckduckgo.com/?t=chatassistsearch&format=json&q='+s, function(results, status) {
+            if (status !== 200) {
+                console.log("Duckduckgo search error");
+                tryWikipedia(socket, mdata, s);  
             } else {
-                res.setEncoding('utf8');
-                
-                res.on('data', function(chunk) { 
-                    results += chunk; 
-                });
-
-                res.on('end', function() {
-                    try {
-                        var data = JSON.parse(results);
-                        
-                        if (data.Image!='') {
-                            m+=data.Image+"#iconsmall\n";
-                        }
-
-                        if (data.AnswerType!='') {
-                            m+=data.Answer;
-                        } else if (data.DefinitionSource!='') {
-                            m+=data.Definition+" [ "+data.DefinitionURL+" ]";
-                        } else if (data.AbstractSource!='') {
-                            m+=data.Abstract+" [ "+data.AbstractURL+" ]";
-                        }
-
-                        if (m=="")
-                            m = "No quick results found. Try https://www.duckduckgo.com/?q="+s;
-                        else
-                            m += " (via DuckDuckGo: https://www.duckduckgo.com/?q="+s+")";
-
-                        socket.emit('chatm', JSON.stringify({"t": token, "room": mdata.room, "m": m, "dur":"1h"}));
-                    } catch (e) {
-                        console.log(e.message);
-                        socket.emit('chatm', JSON.stringify({"t": token, "room": mdata.room, "m": "Search server error.", "dur":"1m"}));
+                try {
+                    var data = JSON.parse(results);
+                    
+                    if (data.Image!='') {
+                        m+=data.Image+"#iconsmall\n";
                     }
-                });
+
+                    if (data.AnswerType!='' && data.Answer!='') {
+                        m+=data.Answer;
+                    } else if (data.DefinitionSource!='') {
+                        m+=data.Definition+" [ "+data.DefinitionURL+" ]";
+                    } else if (data.AbstractSource!='' && data.Abstract!='') {
+                        m+=data.Abstract+" [ "+data.AbstractURL+" ]";
+                    } else {
+                        tryWikipedia(socket, mdata, s);                            
+                    }
+
+                    if (m!="") {
+                        m += " (via DuckDuckGo: https://www.duckduckgo.com/?q="+s+")";                            
+                    }
+
+                    socket.emit('chatm', JSON.stringify({"t": token, "room": mdata.room, "m": m, "dur":"1h"}));
+                } catch (e) {
+                    console.log("Duckduckgo search error",e.message);
+                    tryWikipedia(socket, mdata, s);  
+                }
             }
         });
     }
@@ -71,3 +65,80 @@ exports.onmessage = function(socket, mdata) {
 exports.onroomjoin = function(socket, name, room) {
     console.log("Room ID", room, name);
 };
+
+
+function tryWikipedia(socket, mdata, s) {
+    var requrl="https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&utf8=1&srsearch="+s+"&srlimit=1&srprop=&srenablerewrites=1";
+    var m="", results="";
+    _gethttps(requrl, function(results, status) {
+        if (status !== 200) {
+            socket.emit('chatm', JSON.stringify({"t": token, "room": mdata.room, "m": "Wikipedia search server error.", "dur":"1m"}));
+        } else {
+            try {
+                var data = JSON.parse(results);
+                if (data.query.search.length==0) {
+                    m = "No quick results found. Try https://www.duckduckgo.com/?q="+s;
+                    socket.emit('chatm', JSON.stringify({"t": token, "room": mdata.room, "m": m, "dur":"1m"}));
+                } else { 
+                    getWikipediaArticle(socket, mdata, data.query.search[0].title, s);
+                }
+            } catch (e) {
+                console.log(e.message);
+                socket.emit('chatm', JSON.stringify({"t": token, "room": mdata.room, "m": "Wikipedia search json error.", "dur":"1m"}));
+            }
+        }
+    });
+}
+
+function getWikipediaArticle(socket, mdata, title, s) {
+    var requrl = "https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exlimit=max&explaintext&exintro&titles="+title+"&redirects=";
+    var m="";
+    _gethttps(requrl, function(results, status){
+        if (status!=200) {
+            socket.emit('chatm', JSON.stringify({"t": token, "room": mdata.room, "m": "Wikipedia search server error.", "dur":"1m"}));
+        } else {
+            try {
+                var data = JSON.parse(results);                
+                for (var p in data.query.pages) {
+                    m=data.query.pages[p].extract;
+                    break;
+                }
+
+                if (m!="") {
+                    m += " (via Wikipedia: https://en.wikipedia.org/wiki/"+escape(title.replace(/ /g,'_'))+")";  
+                    socket.emit('chatm', JSON.stringify({"t": token, "room": mdata.room, "m": m, "dur":"1h"}));
+                } else {
+                    m = "No quick results found. Try https://www.duckduckgo.com/?q="+s;
+                    socket.emit('chatm', JSON.stringify({"t": token, "room": mdata.room, "m": m, "dur":"1m"}));
+                }
+                
+
+            } catch (e) {
+                console.log(e.message);
+                socket.emit('chatm', JSON.stringify({"t": token, "room": mdata.room, "m": "Wikipedia search json error.", "dur":"1m"}));
+            }
+        }
+    });
+}
+
+function _gethttps(url, finish_callback) {
+    var results="";
+    https.get(url, function(res) {
+        if (res.statusCode !== 200) {
+            finish_callback(null, res.statusCode);
+        } else {
+            res.setEncoding('utf8');
+            
+            res.on('data', function(chunk) { 
+                results += chunk; 
+            });
+
+            res.on('end', function() {
+                finish_callback(results, res.statusCode);
+            });
+        }
+    }).on('error', function(e) {
+        console.log(e.message);
+        finish_callback(null, 0);
+    });
+}
